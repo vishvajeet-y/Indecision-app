0c46931b216e2011e63dc8f6eaf30bc7ad1f422e
vishvajeet-y <vishvaj_bt18@iiitkalyani.ac.in>
2021-04-30 18:36:53 +0530

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,50 @@
 //entry -> output
 const path=require('path')
-
-module.exports={
-    mode: 'development',
-    entry:"./source/app.js",
-    output:{
-    path:path.join(__dirname,'public'),
-    filename:'bundle.js'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports=(env,argv)=>{
+    const isProduction = argv.mode === 'production'
+  return  {
+        mode: 'development',
+        entry:"./source/app.js",
+        output:{
+        path:path.join(__dirname,'public'),
+        filename:'bundle.js'
+        },
+    
+    module:{
+        rules:[{
+            loader:'babel-loader',
+            test:/\.js$/,
+            exclude:/node_modules/
+       
+        },
+        {
+            test: /\.s?css$/i,
+            use: [MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }
+            ],
+        }
+    ]
     },
-
-module:{
-    rules:[{
-        loader:'babel-loader',
-        test:/\.js$/,
-        exclude:/node_modules/
-   
-    },
-    {
-        test: /\.s?css$/i,
-        use: ["style-loader", "css-loader","sass-loader"],
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename:'style.css'
+        }),
+    ],
+    devtool:isProduction? 'source-map': 'inline-cheap-module-source-map',
+    devServer:{
+        contentBase:path.join(__dirname,'public')
     }
-]
-},
-devtool: 'eval-cheap-module-source-map',
-devServer:{
-    contentBase:path.join(__dirname,'public')
-}
+    }
 }
